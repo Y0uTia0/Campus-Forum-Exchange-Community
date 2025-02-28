858ed3616a09bdd38f0734248042d0994bc65e76
lance <3128970483@qq.com>
2025-02-28 22:12:37 +0800

    frontend-登录界面编写

--- my-project-backend/src/main/java/com/example/config/SecurityConfiguration.java
+++ my-project-backend/src/main/java/com/example/config/SecurityConfiguration.java
@@ -1,62 +1,121 @@
 package com.example.config;
 
 import com.example.entity.RestBean;
-import jakarta.servlet.ServletException;
+import com.example.entity.dto.Account;
+import com.example.entity.vo.response.AuthorizeVO;
+import com.example.filter.JwtAuthorizeFilter;
+import com.example.service.AccountService;
+import com.example.utils.JwtUtils;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.BeanUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Configuration
 public class SecurityConfiguration {
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http)  throws Exception {
-            return http
-                    .authorizeHttpRequests(conf -> conf
-                            .requestMatchers("/api/auth/**").permitAll()
-                            .anyRequest().authenticated()
-                    )
-                    .formLogin(conf -> conf
-                            .loginProcessingUrl("/api/auth/login")
-                            .failureHandler(this::onAuthenticationFailure)
-                            .successHandler(this::onAuthenticationSuccess)
-                    )
-                    .logout(conf -> conf
-                            .logoutUrl("/api/auth/logout")
-                            .logoutSuccessHandler(this::onLogoutSuccess)
-                    )
-                    .csrf(AbstractHttpConfigurer::disable)
-                    .sessionManagement(conf -> conf
-                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                    .build();
-        }
+    @Resource
+    JwtUtils utils;
+
+    @Resource
+    JwtAuthorizeFilter jwtAuthorizeFilter;
+
+    @Resource
+    AccountService service;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(conf -> conf
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(conf -> conf
+                        .loginProcessingUrl("/api/auth/login")
+                        .failureHandler(this::onAuthenticationFailure)
+                        .successHandler(this::onAuthenticationSuccess)
+                )
+                .logout(conf -> conf
+                        .logoutUrl("/api/auth/logout")
+                        .logoutSuccessHandler(this::onLogoutSuccess)
+                )
+                .exceptionHandling(conf -> conf
+                        .authenticationEntryPoint(this::onUnauthorized)
+                        .accessDeniedHandler(this::onAccessDeny)
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(conf -> conf
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthorizeFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+
+
 
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
-            response.getWriter().write(RestBean.success().asJsonString());
+                                        Authentication authentication) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        User user = (User) authentication.getPrincipal();
+        Account account = service.findAccountByNameOrEmail(user.getUsername());
+        String token = utils.createJwt(user, account.getId(), user.getUsername());
+        AuthorizeVO vo = new AuthorizeVO();
+        BeanUtils.copyProperties(account,vo);
+        vo.setExpire(utils.expireTime());
+        vo.setToken(token);
+        response.getWriter().write(RestBean.success(vo).asJsonString());
+    }
+
+
+    public void onLogoutSuccess(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Authentication authentication) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter writer = response.getWriter();
+        String authorization = request.getHeader("Authorization");
+        if (utils.invalidateJwt(authorization)) {
+            writer.write(RestBean.success().asJsonString());
+        }else {
+            writer.write(RestBean.failure(400,"退出登陆失败").asJsonString());
+        }
     }
 
     public void onAuthenticationFailure(HttpServletRequest request,
                                         HttpServletResponse response,
-                                        AuthenticationException exception) throws IOException, ServletException {
+                                        AuthenticationException exception) throws IOException {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(RestBean.failure(401,exception.getMessage()).asJsonString());
+        response.getWriter().write(RestBean.unauthorized(exception.getMessage()).asJsonString());
     }
 
-    public void onLogoutSuccess(HttpServletRequest request,
-                                        HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
+    public void onAccessDeny(HttpServletRequest request,
+                                HttpServletResponse response,
+                                AccessDeniedException exception) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(RestBean.forbidden(exception.getMessage()).asJsonString());
+    }
+
+    public void onUnauthorized(HttpServletRequest request,
+                                HttpServletResponse response,
+                                AuthenticationException exception) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(RestBean.unauthorized(exception.getMessage()).asJsonString());
     }
 }
---
+++ my-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+</script>
+
+<template>
+  <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+
+    <div style="flex:1;background-color:black">
+      <el-image style="width: 100%;height: 100%" fit="cover"
+          src="https://images.steamusercontent.com/ugc/939432803452963615/60637F7A403B0528948FFDAE1BF634E11191562D/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"/>
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 30px;font-weight: bold">欢迎来到我们的学习平台</div>
+      <div style="margin-top: 10px">在这里你可以学习如何使用Java,如何搭建网站,并且与Java支付密切交流</div>
+      <div style="margin-top: 5px">在这里你可以同性交友,因为都是男的,没有学Java的女生</div>
+
+    </div>
+    <div class="right-card">
+<router-view/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.welcome-title {
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+
+
+
+.right-card {
+  width: 400px;
+  z-index: 1;
+  background-color: white;
+}
+</style>
