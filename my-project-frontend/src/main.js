76e4303401ab8ccba066e2eeec7bd274f9004032
lance <3128970483@qq.com>
2025-02-28 23:14:51 +0800

    frontend-Axios请求封装

--- my-project-frontend/src/main.js
+++ my-project-frontend/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from '@/router'
 import axios from "axios";
 
+axios.defaults.baseURL = "http://localhost:8080"
+
 
 const app = createApp(App)
 
--- my-project-frontend/src/views/welcome/LoginPage.vue
+++ my-project-frontend/src/views/welcome/LoginPage.vue
@@ -1,17 +1,32 @@
 <script setup>
 import { User , Lock } from '@element-plus/icons-vue'
-import {reactive} from "vue";
+import {reactive, ref} from "vue";
+import {login} from "@/net";
 
+const formRef = ref()
 
 const form = reactive({
   username: '',
   password: '',
-  rememberMe: false
+  remember: false
 })
 
+const rule = {
+  username: [
+    { required: true, message: '请输入用户名' }
+  ],
+  password: [
+    { required: true, message: '请输入密码'}
+  ]
+}
 
-
-
+function userLogin() {
+  formRef.value.validate((valid) => {
+    if(valid) {
+      login(form.username, form.password, form.remember, () => {})
+    }
+  })
+}
 
 
 </script>
@@ -23,8 +38,8 @@ const form = reactive({
       <div style="font-size: 14px;color: grey">在进入系统之前,请先输入用户名和密码进行登录</div>
     </div>
     <div style="margin-top: 50px">
-      <el-form v-model="form">
-        <el-form-item>
+      <el-form :model="form" :rules="rule" ref="formRef">
+        <el-form-item prop="username">
           <el-input v-model="form.username" maxlength="10" type="text" placeholder="用户名/邮箱">
             <template #prefix>
               <el-icon>
@@ -33,8 +48,8 @@ const form = reactive({
             </template>
           </el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input v-model="form.password" maxlength="20" placeholder="密码">
+        <el-form-item prop="password">
+          <el-input v-model="form.password" type="password" maxlength="20" placeholder="密码">
             <template #prefix>
               <el-icon>
                 <Lock/>
@@ -44,8 +59,8 @@ const form = reactive({
         </el-form-item>
         <el-row>
           <el-col :span="12" style="text-align: left">
-            <el-form-item>
-              <el-checkbox v-model="form.rememberMe" label="记住我"/>
+            <el-form-item prop="remember">
+              <el-checkbox v-model="form.remember" label="记住我"/>
             </el-form-item>
           </el-col>
           <el-col :span="12" style="text-align: right">
@@ -55,7 +70,7 @@ const form = reactive({
       </el-form>
     </div>
     <div style="margin-top: 40px">
-      <el-button style="width: 270px" type="success" plain>立即登录</el-button>
+      <el-button @click="userLogin" style="width: 270px" type="success" plain>立即登录</el-button>
     </div>
     <el-divider>
       <span style="font-size: 13px;color: grey">没有账号</span>
