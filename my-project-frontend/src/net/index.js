822257c7fca319b7965f9f420ba65e7bc7946d69
lance <3128970483@qq.com>
2025-03-02 16:28:24 +0800

    frontend-退出登录以及路由守卫实现

--- my-project-frontend/src/net/index.js
+++ my-project-frontend/src/net/index.js
@@ -16,9 +16,9 @@ const defaultError = (err) => {
 
 function takeAccessToken() {
     const str = localStorage.getItem(authItemName) || sessionStorage.getItem(authItemName);
-    if(!str) return null
+    if (!str) return null
     const authObj = JSON.parse(str)
-    if(authObj.expire <= new Date()) {
+    if (authObj.expire <= new Date()) {
         deleteAccessToken()
         ElMessage.warning("登录状态已过期，请重新登录！")
         return null
@@ -26,13 +26,13 @@ function takeAccessToken() {
     return authObj.token
 }
 
-function storeAccessToken(remember, token, expire){
+function storeAccessToken(remember, token, expire) {
     const authObj = {
         token: token,
         expire: expire
     }
     const str = JSON.stringify(authObj)
-    if(remember)
+    if (remember)
         localStorage.setItem(authItemName, str)
     else
         sessionStorage.setItem(authItemName, str)
@@ -43,9 +43,16 @@ function deleteAccessToken() {
     sessionStorage.removeItem(authItemName)
 }
 
-function internalPost(url, data, header, success, failure, error = defaultError){
-    axios.post(url, data, { headers: header }).then(({data}) => {
-        if(data.code === 200) {
+function accessHeader() {
+    const token = takeAccessToken();
+    return token ? {
+        'Authorization': `Bearer ${takeAccessToken()}`
+    } : {}
+}
+
+function internalPost(url, data, header, success, failure, error = defaultError) {
+    axios.post(url, data, {headers: header}).then(({data}) => {
+        if (data.code === 200) {
             success(data.data)
         } else {
             failure(data.message, data.code, url)
@@ -53,9 +60,9 @@ function internalPost(url, data, header, success, failure, error = defaultError)
     }).catch(err => error(err))
 }
 
-function internalGet(url, header, success, failure, error = defaultError){
-    axios.get(url, { headers: header }).then(({data}) => {
-        if(data.code === 200) {
+function internalGet(url, header, success, failure, error = defaultError) {
+    axios.get(url, {headers: header}).then(({data}) => {
+        if (data.code === 200) {
             success(data.data)
         } else {
             failure(data.message, data.code, url)
@@ -63,7 +70,15 @@ function internalGet(url, header, success, failure, error = defaultError){
     }).catch(err => error(err))
 }
 
-function login(username, password, remember, success, failure = defaultFailure){
+function get(url, success, failure = defaultFailure) {
+    internalGet(url, accessHeader(), success, failure)
+}
+
+function post(url, data, success, failure = defaultFailure) {
+    internalPost(url, data, accessHeader(), success, failure)
+}
+
+function login(username, password, remember, success, failure = defaultFailure) {
     internalPost('/api/auth/login', {
         username: username,
         password: password
@@ -76,4 +91,16 @@ function login(username, password, remember, success, failure = defaultFailure){
     }, failure)
 }
 
-export { login }
+function logout(success, failure = defaultFailure) {
+    get('/api/auth/logout', () => {
+        deleteAccessToken()
+        ElMessage.success('退出登陆成功,欢迎您再次使用')
+        success()
+    }, failure)
+}
+
+function unauthorized() {
+    return !takeAccessToken()
+}
+
+export {login, logout, get, post, unauthorized}
