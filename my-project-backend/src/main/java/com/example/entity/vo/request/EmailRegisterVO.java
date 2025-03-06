26e3419c523d741ee63356ca5bb9ce7ebf9df6f7
lance <3128970483@qq.com>
2025-03-06 12:07:51 +0800

    frontend-完善注册操作

--- my-project-backend/src/main/java/com/example/entity/vo/request/EmailRegisterVO.java
+++ my-project-backend/src/main/java/com/example/entity/vo/request/EmailRegisterVO.java
@@ -9,8 +9,9 @@ import org.hibernate.validator.constraints.Length;
 public class EmailRegisterVO {
 
     @Email
+    @Length(min = 4)
     String email;
-    @Length(min = 6,max = 6)
+    @Length(min = 6, max = 6)
     String code;
     @Pattern(regexp = "^[a-zA-Z0-9\\u4e00-\\u9fa5]+$")
     @Length(min = 1, max = 10)
--- my-project-frontend/src/views/welcome/RegisterPage.vue
+++ my-project-frontend/src/views/welcome/RegisterPage.vue
@@ -2,7 +2,9 @@
 
 import {EditPen, Lock, Message, User} from "@element-plus/icons-vue";
 import router from "@/router";
-import {reactive} from "vue";
+import {reactive, computed, ref} from "vue";
+import {ElMessage} from "element-plus";
+import {get, post} from "@/net";
 
 
 const form = reactive({
@@ -13,20 +15,92 @@ const form = reactive({
   code: ""
 })
 
-const rule = {}
 
+const coldTime = ref(0)
+const formRef = ref()
+
+
+const validateUsername = (rule, value, callback) => {
+  if (value === '') {
+    callback(new Error('请输入用户名'))
+  } else if (!/^[a-zA-Z0-9\u4e00-\u9fa5]+$/.test(value)) {
+    callback(new Error('用户名不能包含特殊字符，只能是中文/英文'))
+  } else {
+    callback()
+  }
+}
+const validatePassword = (rule, value, callback) => {
+  if (value === '') {
+    callback(new Error('请再次输入密码'))
+  } else if (value !== form.password) {
+    callback(new Error("两次输入的密码不一致"))
+  } else {
+    callback()
+  }
+}
+
+const rule = {
+  username: [
+    {validator: validateUsername, trigger: ['blur', 'change']},
+  ],
+  password: [
+    {required: true, message: '请输入密码', trigger: 'blur'},
+    {min: 6, max: 20, message: '密码的长度必须在6-20个字符之间', trigger: ['blur', 'change']}
+  ],
+  password_repeat: [
+    {validator: validatePassword, trigger: ['blur', 'change']},
+  ],
+  email: [
+    {required: true, message: '请输入邮件地址', trigger: 'blur'},
+    {type: 'email', message: '请输入合法的电子邮件地址', trigger: ['blur', 'change']}
+  ],
+  code: [
+    {required: true, message: '请输入获取的验证码', trigger: 'blur'},
+  ]
+}
+
+function askCode() {
+  if (isEmailValid) {
+    coldTime.value = 60
+    get(`/api/auth/ask-code?email=${form.email}&type=register`, () => {
+      ElMessage.success(`验证码已发送到邮箱: ${form.email},请注意查收`)
+      setInterval(() => coldTime.value--, 1000)
+    }, (message) => {
+      ElMessage.warning(message)
+      coldTime.value = 0
+    })
+  } else {
+    ElMessage.warning("请输入正确的电子邮件")
+  }
+}
+
+const isEmailValid = computed(() => /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(form.email))
+
+
+function register() {
+  formRef.value.validate((valid) => {
+    if (valid) {
+      post("/api/auth/register", {...form}, () => {
+        ElMessage.success("注册成功，欢迎加入我们")
+        router.push("/")
+      })
+    } else {
+      ElMessage.warning('请完整填写注册表单内容')
+    }
+  });
+}
 
 </script>
 
 <template>
-  <div style="text-align: center;marrgin: 0 20 px">
+  <div style="text-align: center;margin: 0 20px">
     <div style="margin-top: 100px">
       <div style="font-size: 25px;font-weight: bold">注册新用户</div>
       <div style="font-size: 14px;color: grey">欢迎注册我们的学习平台，请在下方填写相关信息</div>
     </div>
     <div style="margin-top: 50px">
-      <el-form :model="form" :rules="rule" style="width: 360px; margin: 0 auto">
-        <el-form-item>
+      <el-form :model="form" :rules="rule" ref="formRef">
+        <el-form-item prop="username">
           <el-input v-model="form.username" maxlength="10" type="text" placeholder="用户名">
             <template #prefix>
               <el-icon>
@@ -35,7 +109,7 @@ const rule = {}
             </template>
           </el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input v-model="form.password" maxlength="20" type="password" placeholder="密码">
             <template #prefix>
               <el-icon>
@@ -44,7 +118,7 @@ const rule = {}
             </template>
           </el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="password_repeat">
           <el-input v-model="form.password_repeat" maxlength="20" type="password" placeholder="重复密码">
             <template #prefix>
               <el-icon>
@@ -53,8 +127,8 @@ const rule = {}
             </template>
           </el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input v-model="form.email" maxlength="10" type="email" placeholder="电子邮件地址">
+        <el-form-item prop="email">
+          <el-input v-model="form.email" type="email" placeholder="电子邮件地址">
             <template #prefix>
               <el-icon>
                 <Message/>
@@ -62,7 +136,7 @@ const rule = {}
             </template>
           </el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-row :gutter="10" style="width: 100%">
             <el-col :span="17">
               <el-input v-model="form.code" :maxlength="6" type="text" placeholder="请输入验证码">
@@ -74,8 +148,8 @@ const rule = {}
               </el-input>
             </el-col>
             <el-col :span="5">
-              <el-button type="success">
-                获取验证码
+              <el-button @click="askCode" type="success" :disabled="!isEmailValid || coldTime">
+                {{ coldTime > 0 ? `请稍后${coldTime}秒` : '获取验证码' }}
               </el-button>
             </el-col>
           </el-row>
@@ -83,11 +157,11 @@ const rule = {}
       </el-form>
     </div>
     <div style="margin-top: 80px">
-      <el-button style="width: 270px" type="warning"  plain>立即注册</el-button>
+      <el-button @click="register" style="width: 270px" type="warning" plain>立即注册</el-button>
     </div>
     <div style="margin-top: 20px">
       <span style="font-size: 14px;line-height: 15px;color: grey">已有账号? </span>
-      <el-link  style="translate: 0 -1px" @click="router.push('/')">立即登录</el-link>
+      <el-link style="translate: 0 -1px" @click="router.push('/')">立即登录</el-link>
     </div>
   </div>
 </template>
