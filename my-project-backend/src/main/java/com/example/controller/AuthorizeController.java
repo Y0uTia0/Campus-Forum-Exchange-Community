56237182335776960a5eb12b00578a0a1d466530
lance <3128970483@qq.com>
2025-03-06 12:47:40 +0800

    backend-密码重置接口

--- my-project-backend/src/main/java/com/example/controller/AuthorizeController.java
+++ my-project-backend/src/main/java/com/example/controller/AuthorizeController.java
@@ -1,7 +1,9 @@
 package com.example.controller;
 
 import com.example.entity.RestBean;
+import com.example.entity.vo.request.ConfirmResetVO;
 import com.example.entity.vo.request.EmailRegisterVO;
+import com.example.entity.vo.request.EmailResetVo;
 import com.example.service.AccountService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,6 +13,7 @@ import jakarta.validation.constraints.Pattern;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Validated
@@ -30,7 +33,21 @@ public class AuthorizeController {
 
     @PostMapping("/register")
     public RestBean<Void> register(@RequestBody @Valid EmailRegisterVO vo){
-        return this.messageHandle(() -> service.registerEmailAccount(vo));
+        return this.messageHandle(vo,service::registerEmailAccount);
+    }
+
+    @PostMapping("/reset-confirm")
+    public RestBean<Void> resetConfirm(@RequestBody @Valid ConfirmResetVO vo){
+        return this.messageHandle(vo,service::resetConfirm);
+    }
+
+    @PostMapping("/reset-password")
+    public RestBean<Void> resetConfirm(@RequestBody @Valid EmailResetVo vo){
+        return this.messageHandle(vo,service::resetEmailAccountPassword);
+    }
+
+    private <T> RestBean<Void> messageHandle(T vo, Function<T,String> function){
+        return messageHandle(() -> function.apply(vo));
     }
 
     private RestBean<Void> messageHandle(Supplier<String> action){
